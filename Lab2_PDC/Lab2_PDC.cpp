194b756f934cbbe1b0f98fb5ea553d9b7fdb4e07
RomanDunevych <rdunevych@gmail.com>
2024-09-21 19:25:07 +0300

    first commit

---
+++ Lab2_PDC/Lab2_PDC.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Функція для виконання операції XOR
+bool XOR(bool a, bool b) {
+    return a != b;
+}
+
+// Функція fn, яка реалізує логіку p ∧ (q ⊕ r)
+bool fn(bool p, bool q, bool r) {
+    return p && XOR(q, r);
+}
+
+int main() {
+    bool p, q, r;
+
+    cout << "p\tq\tr\tfn(p, q, r) = p ∧ (q ⊕ r)\n";
+    cout << "--------------------------------------\n";
+
+    // Перебір всіх можливих значень p, q, r
+    for (int i = 0; i < 2; ++i) {
+        p = i;
+        for (int j = 0; j < 2; ++j) {
+            q = j;
+            for (int k = 0; k < 2; ++k) {
+                r = k;
+                bool result = fn(p, q, r);
+                cout << p << "\t" << q << "\t" << r << "\t" << result << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		bool XOR(bool a, bool b) {
+			return a != b;
+		}
+
+		bool fn(bool p, bool q, bool r) {
+			return p && XOR(q, r);
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+
+			Assert::AreEqual(fn(0, 0, 0), false);
+			Assert::AreEqual(fn(0, 0, 1), false);
+			Assert::AreEqual(fn(0, 1, 0), false);
+			Assert::AreEqual(fn(0, 1, 1), false);
+			Assert::AreEqual(fn(1, 0, 0), false);
+			Assert::AreEqual(fn(1, 0, 1), true);
+			Assert::AreEqual(fn(1, 1, 0), true);
+			Assert::AreEqual(fn(1, 1, 1), false);
+		}
+	};
+}
